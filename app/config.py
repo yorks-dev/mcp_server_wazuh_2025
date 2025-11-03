3a44657be82875a2fc7f4612609d5c330dc862a4
Preetam Mukherjee <21f1002436@ds.study.iitm.ac.in>
2025-11-03 13:39:34 +0530

    main.py file updated again

--- app/config.py
+++ app/config.py
@@ -25,12 +25,16 @@ class Settings(BaseSettings):
     WAZUH_VERIFY_SSL: bool = False
 
     OPENAI_API_KEY: str = "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"
+ 
 
 
     class Config:
         env_file = ".env"
 
 settings = Settings()
+
+
+
 
 
 
