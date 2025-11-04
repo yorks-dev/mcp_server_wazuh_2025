4066c6fe98091c9b1926c8e5b2ec7afe22a3f7e3
Preetam Mukherjee <21f1002436@ds.study.iitm.ac.in>
2025-11-04 16:52:57 +0530

    Update config.py
    config file updated again.

--- app/config.py
+++ app/config.py
@@ -14,24 +14,31 @@ class Settings(BaseSettings):
     MAX_LIMIT: int = 200
     DEFAULT_TZ: str = "UTC"
 
-    WAZUH_API_HOST: str = "localhost"
-    WAZUH_API_PORT: int = 55000
+     # 🧠 Update these fields for your remote Wazuh Server
+    WAZUH_API_HOST: str = "https://192.168.71.203"
+    WAZUH_API_PORT: int = 1514
     WAZUH_API_USERNAME: str = "wazuh"
-    WAZUH_API_PASSWORD: str = "wazuh"
-    WAZUH_INDEXER_HOST: str = "localhost"
+    WAZUH_API_PASSWORD: str = "wazuh1234"
+    WAZUH_INDEXER_HOST: str = "https://192.168.71.203"
     WAZUH_INDEXER_PORT: int = 9200
     WAZUH_INDEXER_USERNAME: str = "admin"
     WAZUH_INDEXER_PASSWORD: str = "admin"
     WAZUH_VERIFY_SSL: bool = False
 
     OPENAI_API_KEY: str = "sk-proj-"
- 
 
 
     class Config:
         env_file = ".env"
 
 settings = Settings()
+
+
+
+
+
+
+
 
 
 
